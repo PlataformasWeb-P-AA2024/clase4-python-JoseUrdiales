e5905e74e50a4c88a979e3c6bbb65f861f10f3dd
JoseUrdiales <jose-urdiales@hotmail.com>
2024-04-29 20:20:20 -0500

    ejercicio 6 y 7

---
+++ ejemplo07.py
@@ -0,0 +1,11 @@
+diccionario = {}
+
+diccionario ['nombres'] = "José Luis"
+diccionario ['apellidos'] = "Urdiales Cuenca"
+diccionario ['ciudad'] = "Loja"
+
+print(diccionario.keys())
+# print(diccionario['apellidos']
+
+for x in diccionario.keys():
+	print(diccionario[x])
