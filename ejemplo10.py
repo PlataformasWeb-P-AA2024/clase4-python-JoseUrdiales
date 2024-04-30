8d9cbe8a1062381012e2bbb13a1cb9d27f222b44
JoseUrdiales <jlurdiales1@utpl.edu.ec>
2024-04-30 10:43:31 -0500

    ejemplo 10 con nombres únicos por cada archivo, total 25362 archivos

--- ejemplo10.py
+++ ejemplo10.py
@@ -2,11 +2,15 @@ archivo = open("data/atp_tennis.csv", "r")
 
 lineas = archivo.readlines()
 lineas = [l.split("|") for l in lineas]
-print(len(lineas)) #para saber cuantas lineas existen en el csv
 
-for i, x in enumerate(lineas):
-	cadena = """<b>Torneo:</b> %s <br> <b>Ganador:</b> %s""" %(x[0], x[9])
-	print(cadena)
-	archivo_generado = open("data/ganador_%d.html" % (i + 1), "w")
-	archivo_generado.writelines("%s\n" %(cadena))
-	archivo_generado.close()
+for i, x in enumerate(lineas[1:]): 
+    #el lineas[1:] es para que omita la primera linea del csv que son los encabezados, 
+    #así solo salen 25362 archivos en vez de 25363
+    
+    cadena = """<b>Torneo:</b> %s <br> <b>Ganador:</b> %s""" % (x[0], x[9])
+    print(cadena)
+    archivo_generado = open("data/ganador_%d_%s.html" % (i + 1, x[9]), "w")
+    archivo_generado.writelines("%s\n" % cadena)
+    archivo_generado.close()
+    
+print(len(lineas)) #para saber cuantas líneas existen en el csv
